a18defa6251762beec694ef391f2420ce81c8fc3
renxueke <renxueke1010@163.com>
2020-04-09 14:33:28 +0800

    完成了作业一

--- day2_hw.py
+++ day2_hw.py
@@ -19,26 +19,28 @@
 # @permit
 # def test(info)
 #     return data = "123"
+
+# 实现permit装饰器对权限进行验证
+def permit(username,passwd):
+    def login_in(func):
+        def deco(*args, **kwargs):
+            if username=="renxueke"and passwd == "123":
+                print("你有权限")
+            else:
+                print("你没有权限")
+                return
+            func(*args, **kwargs)
+        return deco
+    return login_in
+
+@permit("renxueke","123")
+def login():
+    print("登录成功")
+login()
+
 # 作业二：
-# 递归函数列出所有文件
-# 使用os.listdir
-# os.isfile
+# 递归函数列出所有文件 使用os.listdir os.isfile
 # 练习找出单个目录中的最大文件
 # 练习找出目录树中的最大文件
 
-
-# 实现permit装饰器对权限进行验证
-
-def login_in(passwd):
-    if passwd == "123":
-        print("登录成功")
-    else:
-        print("登录不成功")
-    def data_test(login_in):
-        login_in.passwd = input("请输入密码：")
-@login_in
-def login():
-    print("please input:")
-    print("login")
-login_in(login)
-
+import os
