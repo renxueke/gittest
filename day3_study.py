02bc47297ca383874633ca2932b96b074f9c1a7f
renxueke <renxueke1010@163.com>
2020-04-12 16:46:07 +0800

    第三天学习内容

--- day3_study.py
+++ day3_study.py
@@ -228,11 +228,11 @@
 #     pool.join()
 
 # pool.map
-from multiprocessing import Pool
-import time
-def f(x):
-    time.sleep(0.5)
-    return x*x
-if __name__ == '__main__':
-    with Pool(5)as p:
-        print(p.map(f,range(10)))
+# from multiprocessing import Pool
+# import time
+# def f(x):
+#     time.sleep(0.5)
+#     return x*x
+# if __name__ == '__main__':
+#     with Pool(5)as p:
+#         print(p.map(f,range(10)))
---
+++ day4_study.py
@@ -0,0 +1,16 @@
+# 域名： www.baidu.com
+
+# URL 格式
+#
+# <scheme>://<user>:<password>@<host>:<port>/<path>;<params>?<query>#<frag>
+# <方案>://<用户>:<密码>@<主机>:<端口>/<路径>;<参数>?<查询>#<片段>
+#
+
+#使用python解析url
+# from urllib.parse import urlparse
+# obj=urlparse("https://172.168.10.0:8080/item/统一资源定位系统/5937042?fromtitle=url&fromid=110640")
+# print(obj)
+# print(obj.path)
+# print(obj.scheme)
+# print(obj.query)
+
